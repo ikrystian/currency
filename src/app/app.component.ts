bdfff19536e79be077a091bd102e0a90c08e6e24
Krystian <krystian.kuzminski@programa.pl>
2023-07-04 13:01:51 +0200

    Add currencies form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from './currency.service';
+
+interface Rates {
+  [key: string]: number;
+}
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  amount = 1;
+  toCurrency = 'AED';
+  rates: Rates | undefined;
+  convertedAmount: number | undefined;
 
+  constructor(private currencyService: CurrencyService) {}
+
+  ngOnInit() {
+    this.loadRates();
+    this.convert();
+  }
+
+  loadRates(): void {
+    const storedRates = localStorage.getItem('currencies');
+    if (storedRates) {
+      this.rates = JSON.parse(storedRates);
+    } else {
+      this.currencyService.getRates().subscribe((data) => {
+        this.rates = data.rates;
+        if(this.rates) {
+          this.storeRates(this.rates);
+        }
+      });
+    }
+  }
+
+  storeRates(rates: Rates): void {
+    localStorage.setItem('currencies', JSON.stringify(rates));
+  }
+
+  convert(): void {
+    if(this.rates) {
+      this.convertedAmount = this.amount * this.rates[this.toCurrency];
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule
-    ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
